aac32797a1516c3a8d10d7f22f7ddcebc93e079c
Madhusudan <madhumss0404@gmail.com>
2026-07-07 20:35:20 +0530

    this is commit from GitDemoUdemy guy

--- src/test/java/HTTPAndSSLCertifications/Proxy.java
+++ src/test/java/HTTPAndSSLCertifications/Proxy.java
@@ -1,22 +1,12 @@
 package HTTPAndSSLCertifications;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 public class Proxy {
 	
 	public static void main(String[] args) {
 	
-	ChromeOptions options = new ChromeOptions();
-	Proxy proxy = new Proxy();
-	proxy.setHttpProxy("ipadadress:4444");
-	options.setCapability("proxy", proxy);
-	
-
-
-	WebDriver driver = new ChromeDriver(options);
-	driver.get("https://expired.badssl.com/");
-	System.out.println(driver.getTitle());
+	System.out.println("postJira1");
+	System.out.println("postJira2");
+	System.out.println("postJira3");
 
 }
 }
