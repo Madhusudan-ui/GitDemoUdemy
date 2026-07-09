47dae43234ec9d15b60ea2352ce348b9f94408cd
Madhusudan <madhumss0404@gmail.com>
2026-07-09 11:39:56 +0530

    Architect Asian development

--- src/test/java/HTTPAndSSLCertifications/Proxy.java
+++ src/test/java/HTTPAndSSLCertifications/Proxy.java
@@ -1,12 +1,21 @@
 package HTTPAndSSLCertifications;
 
+import org.testng.annotations.Test;
+
 public class Proxy {
 	
-	public static void main(String[] args) {
-	
+	@Test
+	public void postJira(){
 	System.out.println("postJira1");
 	System.out.println("postJira2");
 	System.out.println("postJira3");
-
 }
+	
+	@Test
+	public void branching()
+	{
+		System.out.println("branch1");
+		System.out.println("branch2");
+		System.out.println("branch3");
+	}
 }
